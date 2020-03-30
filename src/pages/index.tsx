bf2d1c48805033144cf735af1c983a124608754a
‘Yu shigui <yu.shigui@rongzer.com>
2020-03-30 18:01:09 +0800

    'init'

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.less';
+
+export default () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        敬请期待
+      </div>
+    </div>
+  );
+}
